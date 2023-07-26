1a8ca11da6d02cf23a25549dabd60e653697ad5e
YzyLmc <yzy729600559@hotmail.com>
2023-07-26 00:26:48 -0400

    update gpt4 in utils.py

--- utils.py
+++ utils.py
@@ -2,28 +2,36 @@ import openai
 from time import sleep
 import logging
 
-def generate(self, query_prompt):
-    complete = False
-    ntries = 0
-    while not complete:
-        try:
-            raw_responses = openai.Completion.create(
-                model=self.engine,
-                prompt=query_prompt,
-                temperature=self.temp,
-                max_tokens=self.max_tokens,
-                stop=self.stop,
-                n=self.n,
-                # logprobs=5
-            )
-            complete = True
-        except:
-            sleep(30)
-            logging.info(f"{ntries}: waiting for the server. sleep for 30 sec...\n{query_prompt}")
-            logging.info("OK continue")
-            ntries += 1
-    if self.n == 1:
-        responses = [raw_responses["choices"][0]["text"].strip()]
-    else:
-        responses = [choice["text"].strip() for choice in raw_responses["choices"]]
-    return responses
+class GPT4:
+    def __init__(self, engine="gpt-4", temp=0, max_tokens=128, n=1, stop=['\n']):
+        self.engine = engine
+        self.temp = temp
+        self.max_tokens = max_tokens
+        self.n = n
+        self.stop = stop
+
+    def generate(self, query_prompt):
+        complete = False
+        ntries = 0
+        while not complete:
+            try:
+                raw_responses = openai.ChatCompletion.create(
+                    model=self.engine,
+                    messages=prompt2msg(query_prompt),
+                    temperature=self.temp,
+                    n=self.n,
+                    stop=self.stop,
+                    max_tokens=self.max_tokens,
+                )
+                complete = True
+            except:
+                sleep(30)
+                logging.info(f"{ntries}: waiting for the server. sleep for 30 sec...")
+                # logging.info(f"{ntries}: waiting for the server. sleep for 30 sec...\n{query_prompt}")
+                logging.info("OK continue")
+                ntries += 1
+        if self.n == 1:
+            responses = [raw_responses["choices"][0]["message"]["content"].strip()]
+        else:
+            responses = [choice["message"]["content"].strip() for choice in raw_responses["choices"]]
+        return responses
